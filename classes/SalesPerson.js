718d88bbe57bedf5517643b07682e65732d83123
Tatiana Stewart <tstwrt7@gmail.com>
2024-02-07 13:17:00 -0500

    initial commit

--- classes/SalesPerson.js
+++ classes/SalesPerson.js
@@ -1,9 +1,32 @@
-class SalesPerson {
+const Employee = require('./Employees');
 
+class SalesPerson extends Employee{
+    #totalSales
 
+    constructor(name, title, salary, isHired, clients) {
+        super(name, title, salary, isHired);
 
-    
-}
+        this.clients = clients;
+        this.#totalSales = 0;
+    }
+    getSalesNumbers() {
+        return this.#totalSales; 
+    }
+    makeSale(amount) {
+        this.#totalSales += amount;
+        return this.#totalSales;
+    }
+    findClient(client) {
+        let foundClient = null;
+
+        this.clients.forEach(cl => {
+            if (cl === client) {
+                foundClient = cl;
+            }
+        });
+        return foundClient;
+    }
+};
 
 module.exports = {
     SalesPerson,
--- classes/SoftwareEngineer.js
+++ classes/SoftwareEngineer.js
@@ -1,8 +1,20 @@
-class SoftwareEngineer {
+const Employee = require('./Employees');
 
+class SoftwareEngineer extends Employee{
+    #programmingLanguages;
 
+    constructor(name, title, salary, isHired, programmingLanguages) {
+        super(name, title, salary, isHired);
 
-    
+        this.#programmingLanguages = programmingLanguages
+    }
+    getProgrammingLanguages() {
+        return this.#programmingLanguages;
+    }
+    setProgrammingLanguages(language) {
+        this.#programmingLanguages = language;
+        return this.#programmingLanguages;
+    }
 }
 
 module.exports = {
